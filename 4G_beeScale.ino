5c101da897ac9ae1547076ebdbe21abae8379f44
buched <32975584+buched@users.noreply.github.com>
2026-04-28 19:31:56 +0200

    Add copyright and license information to 4G_beeScale.ino
    Added copyright notice and licensing information.

--- 4G_beeScale.ino
+++ 4G_beeScale.ino
@@ -1,3 +1,19 @@
+/*
+ * Copyright (C) 2026  Emmanuel COUVERCELLE (manu@couvercelle.eu)
+ *
+ * Ce programme est un logiciel libre : vous pouvez le redistribuer 
+ * et/ou le modifier selon les termes de la Licence Publique Générale 
+ * GNU telle que publiée par la Free Software Foundation, soit la 
+ * version 3 de la licence, ou (à votre discrétion) toute version ultérieure.
+ *
+ * Ce programme est distribué dans l'espoir qu'il sera utile, mais 
+ * SANS AUCUNE GARANTIE ; sans même la garantie implicite de 
+ * COMMERCIALISATION ou d'ADÉQUATION À UN USAGE PARTICULIER. 
+ * Voir la Licence Publique Générale GNU pour plus de détails.
+ *
+ * Vous devriez avoir reçu une copie de la Licence Publique Générale 
+ * GNU avec ce programme. Sinon, voir <https://www.gnu.org/licenses/>.
+ */
 #define TINY_GSM_MODEM_A7670
 #include "TinyGsmClientfork.h"
 #include <HardwareSerial.h>
